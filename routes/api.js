7e15839e25a8ec780195eacd6176ce175bb85746
AkyunaAkish <akyunaakish@gmail.com>
2016-05-15 15:48:42 -0600

    updated api routes slightly

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,26 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var secret = process.env.SECRET;
 
-/* GET home page. */
-router.get('/something', function(req, res, next) {
-  res.json({ test: 'TEST!!!' });
+// only allow AJAX calls to prevent tampering in the browser bar
+function checkHeaders(req,res,next){
+  if(!req.headers["x-requested-with"]) {
+    res.status(403).json({error: "Not requested with an ajax call"});
+  }
+  else {
+    next();
+  }
+}
+
+router.use(checkHeaders);
+
+router.post('/signup', function(req, res, next) {
+  console.log(req.body);
+  res.status(200).json({ signup: req.body });
+});
+
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
+  res.status(200).json({ login: req.body });
 });
 
 module.exports = router;
